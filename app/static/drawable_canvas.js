3ef794f0c3b7caf94d529f8052c72d37e63cc13b
hurb <harveyraybreeze@gmail.com>
2025-01-05 09:48:34 +0000

    Erase function now works!!!!!

--- app/static/drawable_canvas.js
+++ app/static/drawable_canvas.js
@@ -1,5 +1,5 @@
 const drawable_canvas = document.getElementById("drawingCanvas");
-const drawable_context = drawable_canvas.getContext("2d");
+const drawable_context = drawable_canvas.getContext("2d", {willReadFrequently:true});
 const drawable_bgcol = "#FFFFFF";
 const paper_note_drawcol = (drawable_drawcol) ? "#000000" : drawable_drawcol;
 let drawable_mode = "draw";
@@ -30,13 +30,48 @@ drawable_canvas.addEventListener("touchstart", (e) => {
 
 drawable_canvas.addEventListener("mousemove", (e) => {
 		e.preventDefault();
-		if (drawable_isdrawing) {drawable_context.lineTo(e.offsetX, e.offsetY);drawable_context.stroke();}
+		const x = e.offsetX;
+		const y = e.offsetY;
+		if (drawable_isdrawing) {
+			if (drawable_mode === "draw"){
+				drawable_context.lineTo(x, y);
+				drawable_context.stroke();
+			} else if (drawable_mode === "erase") {
+				const imageData = drawable_context.getImageData(x - 2, y -2, 4, 4);
+				const data = imageData.data;
+				for (let i = 0; i < data.length; i += 4)
+				{
+					data[i] = 0;
+					data[i + 1] = 0;
+					data[i + 2] = 0;
+					data[i + 3] = 0;
+				}
+				drawable_context.putImageData(imageData, x - 2, y - 2);
+			}
+		}
 });
 
 drawable_canvas.addEventListener("touchmove", (e) => {
 		e.preventDefault();
+		erase_size = 3;
 		const {x, y} = get_offset(e.touches[0]);
-		if (drawable_isdrawing) {drawable_context.lineTo(x, y);drawable_context.stroke();}
+		if (drawable_isdrawing) {
+			if (drawable_mode === "draw"){
+				drawable_context.lineTo(x, y);
+				drawable_context.stroke();
+			} else if (drawable_mode === "erase") {
+				const imageData = drawable_context.getImageData(x - erase_size, y - erase_size, erase_size * 2, erase_size * 2);
+				const data = imageData.data;
+				for (let i = 0; i < data.length; i += 4)
+				{
+					data[i] = 0;
+					data[i + 1] = 0;
+					data[i + 2] = 0;
+					data[i + 3] = 0;
+				}
+				drawable_context.putImageData(imageData, x - erase_size, y - erase_size);
+			}
+		}
 });
 
 drawable_canvas.addEventListener("mouseleave", (e) => {
