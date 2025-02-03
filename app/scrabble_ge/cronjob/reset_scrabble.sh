664dab7f7270965d1511eac95595918c4f05ebb5
harvey <harveyraybreeze@gmail.com>
2025-02-03 11:32:30 +0000

    updated scrabble reset cronjob

--- app/scrabble_ge/cronjob/reset_scrabble.sh
+++ app/scrabble_ge/cronjob/reset_scrabble.sh
@@ -1,4 +1,4 @@
 
 source ./.env
 
-wget --header="Authorization: bearer ${ACCESS_TOKEN}" "http://${SRV_ADDRESS}:${SRV_PORT}/word_game/reset" -O /dev/null || touch error.log
+wget --header="Authorization: bearer ${ACCESS_TOKEN}" "https://${SRV_ADDRESS}/word_game/reset" -O /dev/null || touch error.log
